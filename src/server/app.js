bdb4318240b0f3c7d5ea1afa327bba112e57fc55
achieven <achievendar@gmail.com>
2018-04-14 23:13:16 +0300

    added port 8080 to allowed ports for cross origin

--- src/server/app.js
+++ src/server/app.js
@@ -11,6 +11,7 @@ app.listen(port, () => {
 app.use(
     (req, res, next) => {
         res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+        res.header("Access-Control-Allow-Origin", "http://localhost:8080");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     }
