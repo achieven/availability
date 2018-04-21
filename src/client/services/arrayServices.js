49800c0f9e0c3b662d79797531d7dd189b684607
achieven <achievendar@gmail.com>
2018-04-21 17:05:22 +0300

    fixed to show only when within the known range, fixed dividing by zero when no week days are to be calculated

--- src/client/services/arrayServices.js
+++ src/client/services/arrayServices.js
@@ -1,6 +1,6 @@
-export function unshiftArray (array, lastUpdateDay) {
+export function unshiftArray (array, offset) {
     let arrayCopy = array.slice();
-    for (let i = 0; i < lastUpdateDay; i++) {
+    for (let i = 0; i < offset; i++) {
         const lastElement = arrayCopy.pop();
         arrayCopy.unshift(lastElement);
     }
@@ -9,6 +9,9 @@ export function unshiftArray (array, lastUpdateDay) {
 }
 
 export function averageArray (array) {
+    if (0 === array.length) {
+        return 0;
+    }
     const sum = array.reduce(
         (elem1, elem2) => {
             return elem1 + elem2
--- src/client/services/timezoneServices.js
+++ src/client/services/timezoneServices.js
@@ -1,14 +1,23 @@
 import { averageArray, unshiftArray } from "./arrayServices";
 import moment from "moment-timezone";
 
+const weekLength = 7;
+
 export function calculateUserAvailability (lastUpdatedTime, availabilityArray, localTimezone, todayDay) {
     const lastUpdatedDateLocaly = moment.tz(lastUpdatedTime, localTimezone);
     const lastUpdateDayLocally = lastUpdatedDateLocaly.get('days');
-    const todayArrayIndex = (todayDay - lastUpdateDayLocally + 7) % 7;
-    const todaysAvailability = availabilityArray[todayArrayIndex];
-    const normalizedWeekDaysArrayIndex = (lastUpdateDayLocally + 6) % 7;
-    const weekDaysAvailability = unshiftArray(availabilityArray, normalizedWeekDaysArrayIndex).splice(0, 5);
-    const averageAvailability = averageArray(weekDaysAvailability);
+    const todayArrayIndex = (todayDay - lastUpdateDayLocally + weekLength) % weekLength;
+    const availabilityStartingToday = unshiftArray(availabilityArray, weekLength - todayArrayIndex);
+    const todaysAvailability = availabilityStartingToday[0];
+    const averageWeekDaysAvailability = availabilityStartingToday.filter(
+        (availability, index) => {
+            const isWeekDay = [1, 2, 3, 4, 5].includes((index + todayDay) % 7);
+            const isWithinKnownRange = lastUpdateDayLocally === todayDay || (lastUpdateDayLocally - todayDay + 7) % 7 > index;
+            return isWeekDay && isWithinKnownRange;
+        }
+    );
+
+    const averageAvailability = averageArray(averageWeekDaysAvailability);
 
     return {
         todaysAvailability: todaysAvailability,
